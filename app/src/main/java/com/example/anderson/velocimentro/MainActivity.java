1d07db544a927ed4737654d16b7a6d61337cbcb7
anderson <anderson@adm105758l.intranet.ufrb.edu.br>
2016-11-26 19:06:07 -0300

    Initial commit

---
+++ app/src/main/java/com/example/anderson/velocimentro/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.anderson.velocimentro;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    private LocationManager locationMangaer = null;
+    private LocationListener locationListener = null;
+    private TextView t;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        t = (TextView) findViewById(R.id.textView);
+
+        locationMangaer = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationMangaer.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 0, this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        t.setText(String.valueOf(location.getSpeed()));
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
